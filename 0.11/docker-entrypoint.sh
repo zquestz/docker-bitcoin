1e88eda46de98af156108cca0aa3897a358cc454
Adrian Macneil <adrian@adrianmacneil.com>
2015-12-07 15:08:56 -0800

    Allow rpc user/password to be overridden

--- 0.11/docker-entrypoint.sh
+++ 0.11/docker-entrypoint.sh
@@ -6,8 +6,8 @@ if [ "$1" = 'bitcoin-cli' -o "$1" = 'bitcoin-tx' -o "$1" = 'bitcoind' -o "$1" = 
 
   if [ ! -s "$BITCOIN_DATA/bitcoin.conf" ]; then
     cat <<EOF > "$BITCOIN_DATA/bitcoin.conf"
-rpcuser=bitcoin
-rpcpassword=password
+rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 EOF
   fi
 
