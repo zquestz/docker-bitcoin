4d621899199f941628e7ffc193b5c96e7a86821f
Adrian Macneil <adrian@adrianmacneil.com>
2017-07-22 22:15:04 -0700

    Quote bitcoin data directory in entrypoint

--- btc1/1.14.3/docker-entrypoint.sh
+++ btc1/1.14.3/docker-entrypoint.sh
@@ -18,7 +18,7 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
 	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn $BITCOIN_DATA /home/bitcoin/.bitcoin
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
 	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
 
 	exec gosu bitcoin "$@"
