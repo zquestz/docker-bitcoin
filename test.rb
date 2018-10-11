c978b204237c78cff5b926632df1ce8d34891c95
Josh Ellithorpe <quest@mac.com>
2018-10-10 18:25:21 -0700

    Add wormhole support

--- test.rb
+++ test.rb
@@ -9,9 +9,13 @@ def build_image(branch, version, opts)
   # Some clients self-report a different formatted version,
   # so we allow this to be overridden in versions.yml.
   client_version = opts['client_version'] || "v#{version}"
+  opts['binary'] ||= 'bitcoind'
+  opts['binary_cli'] ||= 'bitcoin-cli'
+  opts['binary_tx'] ||= 'bitcoin-tx'
+  opts['binary_test'] ||= 'test_bitcoin'
 
   run "docker build -t #{tag} #{dir}"
-  run %^docker run --rm #{tag} sh -c 'test -n "$(bitcoind -version | grep "version #{client_version}")"'^
+  run %^docker run --rm #{tag} sh -c 'test -n "$(#{opts['binary']} -version | grep "version #{client_version}")"'^
 end
 
 if __FILE__ == $0
